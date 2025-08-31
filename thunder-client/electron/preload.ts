b294de39e455fd449cea300fe7364884bba4660c
isahakgaming-beep <isahak.gaming@gmail.com>
2025-08-31 17:53:04 +0200

    Update preload.ts

--- thunder-client/electron/preload.ts
+++ thunder-client/electron/preload.ts
@@ -1,6 +1,12 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('api', {
-  invoke: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
-  on: (channel: string, listener: (...args: any[]) => void) => ipcRenderer.on(channel, (_e, ...a) => listener(...a)),
+  invoke: (channel: string, args?: any) => ipcRenderer.invoke(channel, args),
+  on: (channel: string, listener: (...args: any[]) => void) => {
+    const sub = (_event: any, ...a: any[]) => listener(...a);
+    ipcRenderer.on(channel, sub);
+    // retourne une fonction pour se désabonner
+    return () => ipcRenderer.removeListener(channel, sub);
+  },
+  ping: () => ipcRenderer.invoke('ping'),
 });
