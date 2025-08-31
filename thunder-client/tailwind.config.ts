632476573f1ee676f5b1c1aa72e403e2c2d878c5
isahakgaming-beep <isahak.gaming@gmail.com>
2025-08-31 17:40:28 +0200

    Update tailwind.config.ts

--- thunder-client/tailwind.config.ts
+++ thunder-client/tailwind.config.ts
@@ -4,10 +4,10 @@ export default {
   darkMode: 'class',
   content: [
     './src/app/**/*.{ts,tsx,js,jsx,mdx}',
-    './src/components/**/*.{ts,tsx,js,jsx}',
+    './src/components/**/*.{ts,tsx,js,jsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
