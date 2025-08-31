4155d64b1c42dce970a77e04fd967da3498d09fd
isahakgaming-beep <isahak.gaming@gmail.com>
2025-08-31 16:56:44 +0200

    Update next.config.js

--- thunder-client/next.config.js
+++ thunder-client/next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-}
+  output: 'export',          // important pour générer /out
+  images: { unoptimized: true }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
