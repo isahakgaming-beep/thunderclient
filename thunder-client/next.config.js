f27d505e91d5dda609223143f507a3b34da61200
zak1 <isahak.gaming@gmail.com>
2025-09-01 13:50:06 +0200

    Update next.config.js

--- thunder-client/next.config.js
+++ thunder-client/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Demande à Next de sortir un site statique dans `out/`
   output: 'export',
-  assetPrefix: './',
-  trailingSlash: true,
+
+  // Si tu utilises <Image/>, ça évite les warnings en mode statique
   images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+
+  // Évite certaines 404 quand on charge des fichiers locaux
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
